dc7fe860ba98b46aeb051e8efd9c285bd52938c6
chenkalili <chen18kalili@gmail.com>
2023-10-30 19:46:04 +0200

    add part1 classes

---
+++ Rebar_project/Rebar_project/Models/Menu.cs
@@ -0,0 +1,15 @@
+﻿using Rebar_project.models;
+
+namespace Rebar_project.Models
+{
+    class Menu
+    {
+        private List<Shake> shakes = new List<Shake>();
+
+        public void AddShake(Shake shake)
+        {
+            shakes.Add(shake);
+            Console.WriteLine($"Shake '{shake._name}' was successfully added to the menu.");
+        }
+    }
+}
---
+++ Rebar_project/Rebar_project/Models/Shake.cs
@@ -0,0 +1,23 @@
+﻿namespace Rebar_project.models
+{
+    class Shake
+    {
+        private static int PersonalID = 0;
+        public int _shakeID { get; }
+        public string _name { get; set; }
+        public string _description { get; set; }
+        public double _priceLarge { get; set; }
+        public double _priceMedium { get; set; }
+        public double _priceSmall { get; set; }
+
+    public Shake(string name, string description, double priceLarge, double priceMedium, double priceSmall)
+        {
+            _shakeID = ++PersonalID;
+            _name = name;
+            _description = description;
+            _priceLarge = priceLarge;
+            _priceMedium = priceMedium;
+            _priceSmall = priceSmall;
+        }
+    }
+}
